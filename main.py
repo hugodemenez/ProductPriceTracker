7a47db6c52d6a127f8fbc1c35e78850d417d80b7
Hugo Demenez <hdemenez@hotmail.fr>
2021-09-13 19:27:11 +0200

    commit

--- main.py
+++ main.py
@@ -20,31 +20,20 @@ class scraping():
         
 
     def get_price(self,url):
-        while(True):
+        self.driver.get(url)
+        for elem in self.driver.find_elements_by_xpath('.//span[@class = "exponent"]'):
             try:
-                self.driver.get(url)
-                for elem in self.driver.find_elements_by_xpath('.//span[@class = "exponent"]'):
-                    try:
-                        if float(elem.text):
-                            unit_price = float(elem.text)
-                    except:
-                        pass
-            except Exception as error:
-                print(f"Il y a une erreur : {error}")
+                if float(elem.text):
+                    self.driver.close()
+                    return float(elem.text)
+            except:
+                pass
+        
+        self.driver.close()
+        return
             
-            try:
-                unit_price
-                self.driver.close()
-                return unit_price
-            except Exception as error:
-                print("Impossible de recupérer le prix, nouvel essai dans 30 secondes")
-                print(f"L'erreur est : {error}")
-                sleep(30)
-                
-        
+            
 
-        
-        
 def envoie_notification(prix_precedent,prix_actuel):
     try:
         token = "1813447727:AAHDPI54DetjXDDNFCMqtN-7phGvwNy9rqY"
@@ -62,12 +51,22 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
     
 if __name__ == "__main__":
-    prix_precedent = scraping().get_price("https://www.boulanger.com/ref/8008595")
+    while(True):
+        try:
+            prix_precedent = float(scraping().get_price("https://www.boulanger.com/ref/8008595"))
+            break
+        except:
+            pass
     print(f"Le prix du produit est actuellement de : {prix_precedent} €")
     while(True):
         if datetime.datetime.now().minute == 0:
             cls()
-            prix_actuel = scraping().get_price("https://www.boulanger.com/ref/8008595")
+            while(True):
+                try:
+                    prix_actuel = float(scraping().get_price("https://www.boulanger.com/ref/8008595"))
+                    break
+                except:
+                    pass
             if prix_precedent != prix_actuel:
                 envoie_notification(prix_precedent,prix_actuel)
                 prix_precedent=prix_actuel
